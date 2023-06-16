2234e3429e8139ec9335b17e74112354088f1eb2
luismartinez375 <luisleonardomartinez375@live.com>
2023-06-16 16:36:39 -0500

    finished lesson02

---
+++ lesson02/src/app/components/contact.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+const Contact = () => {
+    const content = (
+        <main className="max-w-4xl mx-auto p-4">
+            <section id="contact" className="p-6 my-12">
+                <h2 className="text-4xl font-bold text-center sm:text-5xl mb-6 text-slate-900 dark:text-white">
+                    Contact Us
+                </h2>
+            </section>
+            <hr className="mx-auto bg-black dark:bg-white w-1/2"/>
+        </main>
+    )
+    return content
+}
+
+export default Contact;
---
+++ lesson02/src/app/components/main.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Hero from "./hero";
+import Rockets from "./rockets";
+import Testimonials from "./testimonials";
+import Contact from "./contact";
+const Main = () => {
+    const content =(
+       <main>
+        <Hero></Hero>
+        <Rockets></Rockets>
+        <Testimonials></Testimonials>
+        <Contact></Contact>
+       </main>
+    )
+    return content
+}
+
+export default Main;
